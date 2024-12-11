93f90b93ab840591c9255d29515349a49d1b369d
Valmir Paiva Stachin <valmirpaivacpm@gmail.com>
2024-12-11 10:38:41 -0300

    updates

--- src/actions/data/students/get-students.ts
+++ src/actions/data/students/get-students.ts
@@ -5,7 +5,7 @@ import { ReturnType } from '@/@types/return-type';
 
 export async function getStudents(id?: string): Promise<ReturnType<StudentType[]>> {
   try {
-    const res = await fetch(`${process.env.API_URL}/usuarios${id ? `/${id}` : ''}`);
+    const res = await fetch(`${process.env.API_URL}/usuarios${id ? `/${id}` : ''}`, { cache: 'no-store' });
     const students: StudentType[] = await res.json();
 
     return { ok: true, data: students, message: null };
